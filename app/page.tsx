95286a56c42a2aefa7090eb9b324732bc6353991
Alex Sherman <asherman.ca@gmail.com>
2023-03-06 14:40:43 -0800

    create post component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+
+import AddPost from './components/AddPost'
+
 export default function Home() {
 	return (
 		<main>
 			<h1 className='text-lg my-5'>Hello World</h1>
+			<AddPost />
 		</main>
 	)
 }
