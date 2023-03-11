c65b2e6bde178a5ba872b05e0c66e832096ee7ff
Alex Sherman <asherman.ca@gmail.com>
2023-03-11 15:03:14 -0800

    delete post

--- app/dashboard/MyPosts.tsx
+++ app/dashboard/MyPosts.tsx
@@ -3,19 +3,33 @@
 import axios from 'axios'
 import { useQuery } from '@tanstack/react-query'
 import { AuthPosts } from '../types/AuthPosts'
+import EditPost from './EditPost'
 
-const authPosts = async () => {
+const fetchAuthPosts = async () => {
 	const response = await axios.get('/api/posts/authPosts')
 	return response.data
 }
 
 export default function MyPosts() {
-	const { data, error, isLoading } = useQuery<AuthPosts[]>({
-		queryFn: authPosts,
-		queryKey: ['authPosts'],
+	const { data, error, isLoading } = useQuery<AuthPosts>({
+		queryFn: fetchAuthPosts,
+		queryKey: ['auth-posts'],
 	})
 
+	if (isLoading) return <h1>Posts are loading...</h1>
 	console.log('data', data)
-
-	return <div>My Posts</div>
+	return (
+		<div>
+			{data?.post?.map((post) => (
+				<EditPost
+					id={post.id}
+					key={post.id}
+					avatar={data.image}
+					name={data.name}
+					title={post.title}
+					comments={post.comments}
+				/>
+			))}
+		</div>
+	)
 }
--- app/types/AuthPosts.ts
+++ app/types/AuthPosts.ts
@@ -3,7 +3,7 @@ export type AuthPosts = {
 	id: string
 	image: string
 	name: string
-	posts: {
+	post: {
 		createdAt: string
 		id: string
 		title: string
