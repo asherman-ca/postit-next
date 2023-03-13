c7f8ddd2a8fe0ecba9be4286dd9aa00d9c3e14b3
Alex Sherman <asherman.ca@gmail.com>
2023-03-12 22:10:54 -0700

    delete post typing fix

--- pages/api/posts/deletePost.tsx
+++ pages/api/posts/deletePost.tsx
@@ -1,9 +1,13 @@
 import prisma from '../../../prisma/client'
-import { unstable_getServerSession } from 'next-auth/next'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth/next'
 import { authOptions } from '../auth/[...nextauth]'
 
-export default async function handler(req, res) {
-	const session = await unstable_getServerSession(req, res, authOptions)
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const session = await getServerSession(req, res, authOptions)
 	if (!session) {
 		return res.status(401).json({ message: 'Please signin to create a post.' })
 	}
