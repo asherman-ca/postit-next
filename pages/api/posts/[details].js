3e4ae85742f986f6bc0762b5276359ae0a1b62f8
Alex Sherman <asherman.ca@gmail.com>
2023-03-13 17:52:11 -0700

    removed types from post detail api

--- pages/api/posts/[details].js
+++ pages/api/posts/[details].js
@@ -1,11 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../prisma/client'
 
-export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse
-) {
+export default async function handler(req, res) {
 	if (req.method === 'GET') {
 		try {
 			const data = await prisma.post.findUnique({
